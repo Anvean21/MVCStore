2e3211ed407562cd05e4503f6000f41396438b18
Anvean <evgeniy.kovalenko21@gmail.com>
2021-01-25 17:08:25 +0200

    commit

---
+++ MVCStore/Models/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MVCStore.Domain.Core;
+using MVCStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCStore.Models
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
